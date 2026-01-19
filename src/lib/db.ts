c60aeba1f8abb25b3c555f5467a59abf1e7ef95c
jeevan thapa <jeevanthapa@microsoft.com>
2026-01-19 14:00:27 -0500

    02: Database

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
